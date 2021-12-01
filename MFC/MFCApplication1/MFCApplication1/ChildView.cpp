57c6f127ea0312542a8310320bb1daf5ec07b72d
Vinicius Jarina <viniciusjarina@gmail.com>
2021-12-01 15:41:51 -0800

    Initial commit MFC sample

---
+++ MFC/MFCApplication1/MFCApplication1/ChildView.cpp
@@ -0,0 +1,57 @@
+
+// ChildView.cpp : implementation of the CChildView class
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication1.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+
+// CChildView message handlers
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // device context for painting
+	
+	CRect rc;
+	GetClientRect(&rc);
+	dc.SetDCBrushColor(RGB(212, 212, 220));
+	dc.SelectStockObject(DC_BRUSH);
+	dc.FillRect(rc, dc.GetCurrentBrush());
+}
+
